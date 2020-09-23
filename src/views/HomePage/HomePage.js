1a9314660a9315fff96cd68831c6260bf17dddc1
mikiteek <mikiteek@gmail.com>
2020-09-23 15:23:33 +0300

    Create HomePage component, add route for this one

---
+++ src/views/HomePage/HomePage.js
@@ -0,0 +1,42 @@
+import React, {Component} from "react";
+import {NavLink} from "react-router-dom";
+import MovieList from "../../components/MovieList/MovieList";
+import fetchWithQuery from "../../services/movie-api";
+
+class HomePage extends Component {
+  state = {
+    movies: [],
+    error: null
+  }
+
+  componentDidMount() {
+    console.log("Did mount");
+    fetchWithQuery("trending/movie/day")
+      .then(({results}) => results)
+      .then(movies => this.setState({movies}))
+      .catch(error => this.setState({error}));
+  }
+
+  render() {
+    const {movies, error} = this.state;
+    return (
+      <section className="container">
+        <h2>Trending today</h2>
+        {
+          movies.length > 0 &&
+          <MovieList>
+            {movies.map(film => (
+              <li key={film.id}>
+                <NavLink to="">{film.title}</NavLink>
+              </li>
+            ))}
+          </MovieList>
+        }
+        {error && <h2>For technical reasons the information is temporarily unavailable, please try again later</h2>}
+
+      </section>
+    );
+  }
+}
+
+export default HomePage;
