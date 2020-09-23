73b96149a7bf8757014749d0cde19fa4fe5954fa
mikiteek <mikiteek@gmail.com>
2020-09-23 16:32:21 +0300

    Create MoviePage component, add route for this one

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {Route, Switch} from "react-router-dom";
 import Layout from "./components/Layout/Layout";
 import NavBar from "./components/NavBar/NavBar";
 import HomePage from "./views/HomePage/HomePage";
+import MoviesPage from "./views/MoviesPage/MoviesPage";
 import routes from "./routes";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
 
       <Switch>
         <Route path={routes.home} exact component={HomePage}/>
+        <Route path={routes.movies} component={MoviesPage}/>
       </Switch>
     </Layout>
   );
