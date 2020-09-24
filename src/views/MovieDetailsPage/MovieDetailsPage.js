362a283b28d9ef8d65612f0487345187f235cc1d
mikiteek <mikiteek@gmail.com>
2020-09-24 15:03:33 +0300

    Create Cast view component, implement render on MovieDetailsPage

--- src/views/MovieDetailsPage/MovieDetailsPage.js
+++ src/views/MovieDetailsPage/MovieDetailsPage.js
@@ -1,7 +1,9 @@
 import React, {Component} from "react";
 import MovieDetails from "../../components/MovieDetails/MovieDetails";
+import Cast from "../Cast/Cast";
 import fetchWithQuery from "../../services/movie-api";
 import routes from "../../routes";
+import {NavLink, Route} from "react-router-dom";
 
 class MovieDetailsPage extends Component {
   state = {
@@ -28,10 +30,20 @@ class MovieDetailsPage extends Component {
 
   render() {
     const {movie, error} = this.state;
+    const {match} = this.props;
+    console.log(match);
     return (
       <div className="container">
         <button type="button" onClick={this.handleGoBack}>Return to movies</button>
         {movie && <MovieDetails movie={movie}/>}
+
+        <section>
+          <h2>Additional infirmation</h2>
+          <NavLink to={`${match.url}/cast`}>Cast</NavLink>
+        </section>
+
+        <Route path={`${match.path}/cast`} component={Cast}/>
+
         {error && <h2>For technical reasons the information is temporarily unavailable, please try again later</h2>}
       </div>
     );
